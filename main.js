d90b1f11c8d25e048ffc133adbebf7ba0275e3aa
Jordan Manus <jlmanus17@gmail.com>
2023-10-13 15:13:21 -0400

    removed form from html. added new classes (number/operater) to each button. removed form from css. added in eventlisteners for both numbers and operators.

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
 console.log('connected');
 
+let display = document.querySelector('#display');
+let mainContainer = document.querySelector('.bottom');
+let string = "";
+
+let newDisplay = 5;
+display.innerText = 'hello';
+
+let numbers = document.querySelectorAll('.number');
+for (let number of numbers) {
+    number.addEventListener('click', (event) => {
+        console.log(event.target.innerText);
+        number = event.target.innerText;
+        string += number;
+        console.log(`this is the string: ${string}`);
+    })
+}
+
+let operators = document.querySelectorAll('.operator');
+for (let operator of operators) {
+    operator.addEventListener('click', (event) => {
+        console.log(event.target.innerText);
+        operator = event.target.innerText;
+        string += operator;
+        console.log(`this is the string: ${string}`);
+
+    })
+}
+
+
+// function to assign values to button
+// function defineValueToButton(element) {
+//     switch()
+// }
+
+
+
+
+// how to clear the calculator: element_name.remove();
